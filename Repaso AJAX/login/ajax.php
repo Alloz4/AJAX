d17813062983ddedaeb1c4d024f0168f562584d9
Marcos <marcos.alloza4@gmail.com>
2023-02-16 23:49:37 +0100

    repaso ajax

---
+++ Repaso AJAX/login/ajax.php
@@ -0,0 +1,10 @@
+<?php
+
+$aleatorio = rand(0, 10);
+
+if ($aleatorio % 2 == 0) {
+	echo "Si";
+} else {
+
+	echo "No";
+}
---
+++ Repaso AJAX/sumatorio/asumar.php
@@ -0,0 +1,3 @@
+<?php
+$resultado = $_POST['valor1'] + $_POST['valor2'];
+echo $resultado;
